db3ae1ebf0bda4b53ed88df550ac7bf28285c1ec
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-01 15:25:53 -0600

    added: SubjectRepository

---
+++ src/main/java/org/eduardo/repository/SubjectRepository.java
@@ -0,0 +1,49 @@
+package org.eduardo.repository;
+
+import jakarta.persistence.EntityManager;
+import org.eduardo.entity.Subject;
+
+import javax.swing.*;
+import java.util.List;
+
+public class SubjectRepository implements CrudGenericRepository<Subject>{
+
+    private final EntityManager manager;
+
+    public SubjectRepository(EntityManager manager) {
+        this.manager = manager;
+    }
+    @Override
+    public List<Subject> create() {
+        return manager.createQuery("select s from Subject s", Subject.class).getResultList();
+    }
+
+    @Override
+    public Subject findById(Integer id) {
+        return manager.find(Subject.class,id);
+    }
+
+    @Override
+    public void save(Subject subject) {
+        if (subject.getId()!= null && subject.getId()<0){
+            manager.merge(subject);
+        } else {
+            manager.persist(subject);
+        }
+    }
+
+    @Override
+    public void edit(Integer id) {
+        Subject subject = findById(id);
+        if (subject.getId()!= null && subject.getId()>0){
+            subject.setTitulo(JOptionPane.showInputDialog("Digita el titulo: ", subject.getTitulo()));
+            manager.merge(subject);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Subject subject = findById(id);
+        manager.remove(subject);
+    }
+}
