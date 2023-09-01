7b466f77007fd2ec00f83e920d9f6073344b29a7
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-01 15:41:14 -0600

    added: TeacherRepository

---
+++ src/main/java/org/eduardo/repository/TeacherRepository.java
@@ -0,0 +1,51 @@
+package org.eduardo.repository;
+
+import jakarta.persistence.EntityManager;
+import org.eduardo.entity.Teacher;
+
+import javax.swing.*;
+import java.util.List;
+
+public class TeacherRepository implements CrudGenericRepository<Teacher>{
+
+    private final EntityManager manager;
+
+    public TeacherRepository(EntityManager manager) {
+        this.manager = manager;
+    }
+
+    @Override
+    public List<Teacher> create() {
+        return manager.createQuery("select t from Teacher t", Teacher.class).getResultList();
+    }
+
+    @Override
+    public Teacher findById(Integer id) {
+        return manager.find(Teacher.class, id);
+    }
+
+    @Override
+    public void save(Teacher teacher) {
+        if (teacher.getId()!=null && teacher.getId()>0){
+            manager.merge(teacher);
+        } else {
+            manager.persist(teacher);
+        }
+    }
+
+    @Override
+    public void edit(Integer id) {
+        Teacher teacher = findById(id);
+        if (teacher.getId()!= null && teacher.getId()>0){
+            teacher.setNombre(JOptionPane.showInputDialog("Digita el primer nombre: ", teacher.getNombre()));
+            teacher.setApellido(JOptionPane.showInputDialog("Digita el apellido: ", teacher.getApellido()));
+            manager.merge(teacher);
+        }
+    }
+
+    @Override
+    public void delete(Integer id) {
+        Teacher teacher = findById(id);
+        manager.remove(teacher);
+    }
+}
