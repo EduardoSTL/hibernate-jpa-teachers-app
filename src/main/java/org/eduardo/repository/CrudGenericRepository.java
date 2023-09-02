3d0d272aeff85f52e837f0a63258ef290ad92d0c
EduardoSTL <eduardojosemelgar2004@gmail.com>
2023-09-01 19:18:51 -0600

    update: CrudGenericRepository

--- src/main/java/org/eduardo/repository/CrudGenericRepository.java
+++ src/main/java/org/eduardo/repository/CrudGenericRepository.java
@@ -1,11 +1,13 @@
 package org.eduardo.repository;
 
+import org.eduardo.entity.Teacher;
+
 import java.util.List;
 
 public interface CrudGenericRepository <T>{
     List<T> create();
     T findById(Integer id);
-    void save(T t);
+    T save(T entidad);
     void edit(Integer id);
     void delete(Integer id);
     List<T> findAll();
